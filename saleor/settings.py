56d175bd6684b15f5e25ca18dc0dbaccf46f808d
kunal sharma <kunalsharma.ak23@gmail.com>
2025-07-17 04:54:55 +0530

    fix rsa code.

--- saleor/settings.py
+++ saleor/settings.py
@@ -257,7 +257,7 @@ if not SECRET_KEY and DEBUG:
 RSA_PRIVATE_KEY = None
 decoded_private_key = None
 if (os.environ["RSA_PRIVATE_KEY_BASE64"]):
-    b64_key = os.environ.get("RSA_PRIVATE_KEY_B64")
+    b64_key = os.environ.get("RSA_PRIVATE_KEY_BASE64")
     if b64_key:
         decoded_key = base64.b64decode(b64_key.encode("utf-8"))
         RSA_PRIVATE_KEY = decoded_key.decode("utf-8")
